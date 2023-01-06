a011544068855907393ad72db870f828c91833d8
Alex Casanova <alexcasan@gmail.com>
2023-01-06 17:21:22 -0500

    updated stock to use @properties

--- Work/stock.py
+++ Work/stock.py
@@ -4,6 +4,14 @@ class Stock:
         self.shares = shares
         self.price = price
 
+    @property
+    def shares(self):
+        return self._shares
+    @shares.setter
+    def shares(self, value):
+        if not isinstance(value, int):
+            raise TypeError('Expected int')
+        self._shares = value
     def __repr__(self):
         return 'Stock('+','.join([self.name,str(self.shares),str(self.price)])+')'
 
