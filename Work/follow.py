072f06e4939ced0b93efedf61eae6f5f5380f3c0
Alex Casanova <alexcasan@gmail.com>
2023-01-09 16:43:01 -0500

    updated follow.py

--- Work/follow.py
+++ Work/follow.py
@@ -3,27 +3,28 @@ import os
 import time
 
 def follow(filename):
-
-    f = open('Data/stocklog.csv')
-    f.seek(0, os.SEEK_END)   # Move file pointer 0 bytes from end of file
-
+    '''
+    Generator that produces a sequence of lines being written at the end of a file.
+    '''
+    f = open(filename, 'r')
+    f.seek(0,os.SEEK_END)
     while True:
         line = f.readline()
         if line == '':
-            time.sleep(0.1)   # Sleep briefly and retry
+            time.sleep(0.1)    # Sleep briefly to avoid busy wait
             continue
         yield line
 
+# Example use
+if __name__ == '__main__':
+    import report
 
+    portfolio = report.read_portfolio('Data/portfolio.csv')
 
-while True:
-    line = f.readline()
-    if line == '':
-        time.sleep(0.1)   # Sleep briefly and retry
-        continue
-    fields = line.split(',')
-    name = fields[0].strip('"')
-    price = float(fields[1])
-    change = float(fields[4])
-    if change < 0:
-        print(f'{name:>10s} {price:>10.2f} {change:>10.2f}')
+    for line in follow('Data/stocklog.csv'):
+        row = line.split(',')
+        name = row[0].strip('"')
+        price = float(row[1])
+        change = float(row[4])
+        if name in portfolio:
+            print(f'{name:>10s} {price:>10.2f} {change:>10.2f}')
