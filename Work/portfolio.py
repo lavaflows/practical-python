2b423e27c6cc94ebdecdcff8bac228620adf2a9f
Alex Casanova <alexcasan@gmail.com>
2025-06-16 16:39:04 -0400

    update

--- Work/portfolio.py
+++ Work/portfolio.py
@@ -7,7 +7,7 @@ class Portfolio:
     def __iter__(self):
         return self._holdings.__iter__()
     def __contains__(self, item):
-        return item in [stock.name for stock in self._holdings]
+        return any([item == stock.name for stock in self._holdings])
 
 
     @property
