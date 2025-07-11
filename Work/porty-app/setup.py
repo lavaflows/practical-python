4749fe7372e352a4b6a723dbfb56049fe30e05f0
lavaflows <alexcasan@gmail.com>
2025-07-11 01:10:34 -0400

    feat(finish): add setup

---
+++ Work/porty-app/setup.py
@@ -0,0 +1,11 @@
+# setup.py
+import setuptools
+
+setuptools.setup(
+    name="porty",
+    version="0.1.0",
+    author="Alex Casanova",
+    author_email="alexcasan@gmail.com",
+    description="Practical Python Code",
+    packages=setuptools.find_packages(),
+)
